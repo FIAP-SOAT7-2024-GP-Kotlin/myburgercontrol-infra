8a3d4621edbc7cbfea74b0c7207776ad04f0c017
Rafael Correia <rafael.correia@alloymit.com>
2024-09-17 13:19:06 -0300

    init db script

--- script/k8s-cluster/drop-cluster.sh
+++ script/k8s-cluster/drop-cluster.sh
@@ -6,3 +6,7 @@ doctl kubernetes cluster get $PROJECT_NAME-k8s
 
 echo "Deleting $PROJECT_NAME-k8s Cluster and all resources"
 doctl kubernetes cluster delete --dangerous $PROJECT_NAME-k8s
+
+# Deleting the database
+DBID=`doctl db list -o json | jq ".[0].id"`
+doctl db delete $DBID --force
