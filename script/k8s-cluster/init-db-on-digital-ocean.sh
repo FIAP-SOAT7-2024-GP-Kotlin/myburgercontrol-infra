ac97d2e87e46d04bee39ed980c5273b11d80fc43
Rafael Correia <rafaeljpc@gmail.com>
2024-09-17 02:49:00 -0300

    init db on DO

--- script/k8s-cluster/init-db-on-digital-ocean.sh
+++ script/k8s-cluster/init-db-on-digital-ocean.sh
@@ -10,3 +10,16 @@ doctl databases create \
     -o json \
     myburger-db-server
 
+DBID=`doctl db list -o json | jq ".[0].id"`
+
+doctl db get-ca $DBID -o json | jq -r .certificate | base64 -d > db-do.cert
+
+doctl db list -o json | jq ".[0].connection" > connection-info.json
+
+DB_HOST=`cat connection-info.json | jq ".[0].connection.host"`
+DB_DATABASE=`cat connection-info.json | jq ".[0].connection.database"`
+DB_USER=`cat connection-info.json | jq ".[0].connection.user"`
+DB_PASSWD=`cat connection-info.json | jq ".[0].connection.password"`
+
+cat script/db/my_burger_db_creation.sql | docker run -it --rm postgres -- psql -U $DB_USER -W -h $DB_HOST
+
