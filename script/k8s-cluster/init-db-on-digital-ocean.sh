e033ce4006ce06ab170db314c2e8a06060a4fd88
Rafael Correia <rafael.correia@alloymit.com>
2024-09-17 18:53:32 -0300

    init database ok

--- script/k8s-cluster/init-db-on-digital-ocean.sh
+++ script/k8s-cluster/init-db-on-digital-ocean.sh
@@ -18,15 +18,7 @@ doctl db get-ca $DBID -o json | jq -r .certificate | base64 -d > tmp/db-do.cert
 
 doctl db list -o json | jq ".[0].connection" > tmp/connection-info.json
 
-DB_HOST=`cat ./tmp/connection-info.json | jq ".host"`
-DB_DATABASE=`cat ./tmp/connection-info.json | jq ".database"`
-DB_USER=`cat ./tmp/connection-info.json | jq ".user"`
-DB_PASSWD=`cat ./tmp/connection-info.json | jq ".password"`
+DB_URI=`cat ./tmp/connection-info.json | jq ".uri" -r`
 
-echo "DB_HOST=$DB_HOST"
-echo "DB_DATABASE=$DB_DATABASE"
-echo "DB_USER=$DB_USER"
-echo "DB_PASSWD=$DB_PASSWD"
-
-# cat script/db/my_burger_db_creation.sql | docker run -it --rm postgres -- psql -U $DB_USER -W -h $DB_HOST
+cat script/db/my_burger_db_creation.sql | docker run -i --rm fiapmyburguer/postgresql-client -- $DB_URI
 
