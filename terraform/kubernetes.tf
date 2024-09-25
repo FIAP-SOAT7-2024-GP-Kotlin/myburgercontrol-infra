a854d3751047dc36395e2c60d716af9abb727bf3
FlorencioFer <fur_oliveira@hotmail.com>
2024-09-25 20:06:05 -0300

    format

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -1,12 +1,11 @@
 resource "digitalocean_kubernetes_cluster" "my_burger_kubernetes_cluster" {
   name   = "my-burger-k8s"
   region = "nyc3"
-  # Grab the latest version slug from `doctl kubernetes options versions`
   version = "1.22.8-do.1"
 
   node_pool {
-    name       = "my-burger-node-pool"
-    size       = "s-1vcpu-2gb"
+    name  = "my-burger-node-pool"
+    size  = "s-1vcpu-2gb"
     node_count = 1
     auto_scale = true
     min = 1
@@ -14,8 +13,8 @@ resource "digitalocean_kubernetes_cluster" "my_burger_kubernetes_cluster" {
     vpc_uuid = var.vpc_id
 
     taint {
-      key    = "workloadKind"
-      value  = "database"
+      key = "workloadKind"
+      value = "database"
       effect = "NoSchedule"
     }
   }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,7 +2,7 @@ variable "do_token" {
   type = string
 }
 
-variable "vpc_id"{
+variable "vpc_id" {
   type = string
   default = "a2d6c783-0d5c-47f8-95a4-a75146ad7e9c"
 }
