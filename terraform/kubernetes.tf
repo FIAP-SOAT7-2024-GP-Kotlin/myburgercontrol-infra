cd0536758e41d502071d89922093e80e619ed6eb
FlorencioFer <fur_oliveira@hotmail.com>
2024-09-25 20:42:10 -0300

    fixing kubernetes slug version

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -1,7 +1,7 @@
 resource "digitalocean_kubernetes_cluster" "my_burger_kubernetes_cluster" {
   name     = "my-burger-k8s"
   region   = "nyc3"
-  version  = "1.22.8-do.1"
+  version  = "1.31.1-do.0"
   vpc_uuid = var.vpc_id
 
   node_pool {
