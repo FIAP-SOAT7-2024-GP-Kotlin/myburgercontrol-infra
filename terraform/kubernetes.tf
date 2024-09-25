bc575574418f87c16fb6fc5c83438619474caf7c
FlorencioFer <fur_oliveira@hotmail.com>
2024-09-25 20:10:47 -0300

    fixing format

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -2,20 +2,14 @@ resource "digitalocean_kubernetes_cluster" "my_burger_kubernetes_cluster" {
   name   = "my-burger-k8s"
   region = "nyc3"
   version = "1.22.8-do.1"
+  vpc_uuid = var.vpc_id
 
   node_pool {
     name  = "my-burger-node-pool"
     size  = "s-1vcpu-2gb"
     node_count = 1
     auto_scale = true
-    min = 1
-    max = 3
-    vpc_uuid = var.vpc_id
-
-    taint {
-      key = "workloadKind"
-      value = "database"
-      effect = "NoSchedule"
-    }
+    min_nodes = 1
+    max_nodes = 3
   }
 }
