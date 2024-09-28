34eb3b2055751b1b8a6b33df63ba6f3a92e687e9
FlorencioFer <fur_oliveira@hotmail.com>
2024-09-28 16:57:36 -0300

    adding kubernetes provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.32.0"
+    }
   }
 }
 
