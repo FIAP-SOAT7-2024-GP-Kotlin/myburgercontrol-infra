5dd712fe7082411b563540002331533fc9449bb4
FlorencioFer <fur_oliveira@hotmail.com>
2024-09-29 13:51:17 -0300

    run

--- terraform/db.tf
+++ terraform/db.tf
@@ -1,13 +1,13 @@
-# resource "digitalocean_database_db" "my_burger_database" {
-#   cluster_id = digitalocean_database_cluster.myburger_database_cluster.id
-#   name       = "my_burger"
-# }
+resource "digitalocean_database_db" "my_burger_database" {
+  cluster_id = digitalocean_database_cluster.myburger_database_cluster.id
+  name       = "my_burger"
+}
 
-# resource "digitalocean_database_cluster" "myburger_database_cluster" {
-#   name       = "myburger-db-server"
-#   engine     = "pg"
-#   version    = "16"
-#   size       = "db-s-1vcpu-1gb"
-#   region     = "nyc1"
-#   node_count = 1
-# }
+resource "digitalocean_database_cluster" "myburger_database_cluster" {
+  name       = "myburger-db-server"
+  engine     = "pg"
+  version    = "16"
+  size       = "db-s-1vcpu-1gb"
+  region     = "nyc1"
+  node_count = 1
+}
