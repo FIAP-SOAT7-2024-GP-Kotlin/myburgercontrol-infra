093d1f688931a32fef8b73b78511b56ac31ed9f0
FlorencioFer <fur_oliveira@hotmail.com>
2024-11-21 20:42:19 -0300

    changes

--- script/k8s-cluster/drop-cluster.sh
+++ script/k8s-cluster/drop-cluster.sh
@@ -8,7 +8,9 @@ echo "Deleting $K8S_CLUSTER_NAME Cluster and all resources"
 doctl kubernetes cluster delete --dangerous $K8S_CLUSTER_NAME
 
 # Deleting the database
+echo "Deleting Databases"
 DBID=`doctl db list -o json | jq ".[0].id"`
 doctl db delete $DBID --force
 
+echo "Deleting api-gtw"
 doctl compute droplet delete my-burger-api-gtw
