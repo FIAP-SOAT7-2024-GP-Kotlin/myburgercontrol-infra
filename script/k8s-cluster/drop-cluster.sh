cfccfbbbda6a621a443dc155ea3e83b0647e1b84
FlorencioF <46655493+FlorencioF@users.noreply.github.com>
2024-11-30 14:25:38 -0300

    Feature/fix kong routes (#22)
    * fixing pipeline runs
    
    * fixing deploys and routes
    
    * fix runs and deleting script
    
    * fix script

--- script/k8s-cluster/drop-cluster.sh
+++ script/k8s-cluster/drop-cluster.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-PROJECT_NAME=my-burger
-K8S_CLUSTER_NAME=$PROJECT_NAME-k8s
 
-doctl kubernetes cluster get $K8S_CLUSTER_NAME
+echo "Deleting Kubernetes"
+KUBERNETES_IDS=$(doctl kubernetes cluster list --format ID --no-header)
+for KUBERNETES_IDS in $KUBERNETES_IDS; do
+  echo "Deleting database with ID: $KUBERNETES_IDS"
+  doctl kubernetes cluster delete $KUBERNETES_IDS --force
+done
 
-echo "Deleting $K8S_CLUSTER_NAME Cluster and all resources"
-doctl kubernetes cluster delete --dangerous $K8S_CLUSTER_NAME
-
-# Deleting the database
 echo "Deleting Databases"
 DB_IDS=$(doctl db list --format ID --no-header)
 for DB_ID in $DB_IDS; do
@@ -15,9 +14,16 @@ for DB_ID in $DB_IDS; do
   doctl db delete $DB_ID --force
 done
 
-echo "Deleting api-gtw"
+echo "Deleting Dropletes"
 DROPLET_IDS=$(doctl compute droplet list --format ID --no-header)
 for DROPLET_ID in $DROPLET_IDS; do
   echo "Deleting database with ID: $DROPLET_ID"
   doctl compute droplet delete $DROPLET_ID --force
+done
+
+echo "Deleting Load Balancers"
+LB_IDS=$(doctl compute load-balancer list --format ID --no-header)
+for LB_IDS in $LB_IDS; do
+  echo "Deleting database with ID: $LB_IDS"
+  doctl compute load-balancer delete $LB_IDS --force
 done
