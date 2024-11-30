a58ee11be9c0d9f730e26db7521589e797d6b2fc
FlorencioF <46655493+FlorencioF@users.noreply.github.com>
2024-11-29 23:22:43 -0300

    Feature/fix kong routes (#21)
    * fixing pipeline runs
    
    * fixing deploys and routes

--- script/k8s-cluster/drop-cluster.sh
+++ script/k8s-cluster/drop-cluster.sh
@@ -9,12 +9,15 @@ doctl kubernetes cluster delete --dangerous $K8S_CLUSTER_NAME
 
 # Deleting the database
 echo "Deleting Databases"
-DB_IDS=$(doctl db list -o json | jq -r '.[].id')
-
+DB_IDS=$(doctl db list --format ID --no-header)
 for DB_ID in $DB_IDS; do
   echo "Deleting database with ID: $DB_ID"
   doctl db delete $DB_ID --force
 done
 
 echo "Deleting api-gtw"
-doctl compute droplet delete my-burger-api-gtw
+DROPLET_IDS=$(doctl compute droplet list --format ID --no-header)
+for DROPLET_ID in $DROPLET_IDS; do
+  echo "Deleting database with ID: $DROPLET_ID"
+  doctl compute droplet delete $DROPLET_ID --force
+done
