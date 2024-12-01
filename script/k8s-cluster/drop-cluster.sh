f8e4eb09ddbaed19861c45791f312a059c12ad77
FlorencioF <46655493+FlorencioF@users.noreply.github.com>
2024-12-01 15:54:59 -0300

    Feature/fix kong routes (#32)
    * fixing pipeline runs
    
    * fixing deploys and routes
    
    * fix runs and deleting script
    
    * fix script
    
    * creating LBs and routing to them
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fix kong config
    
    * creating dynamic postgres db uri

--- script/k8s-cluster/drop-cluster.sh
+++ script/k8s-cluster/drop-cluster.sh
@@ -3,7 +3,7 @@
 echo "Deleting Kubernetes"
 KUBERNETES_IDS=$(doctl kubernetes cluster list --format ID --no-header)
 for KUBERNETES_IDS in $KUBERNETES_IDS; do
-  echo "Deleting database with ID: $KUBERNETES_IDS"
+  echo "Deleting Kubernetes with ID: $KUBERNETES_IDS"
   doctl kubernetes cluster delete $KUBERNETES_IDS --force
 done
 
